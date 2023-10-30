84b254890f864564b687be2cbbf17e708ecf751d
Sam <109683132+kernelsam@users.noreply.github.com>
2023-10-30 13:36:18 -0700

    #57 debug

--- aws/docker-build.sh
+++ aws/docker-build.sh
@@ -13,7 +13,6 @@ usage() {
  echo "Options:"
  echo " -h, --help      Display this help message"
  echo " -a, --args      Additional build arguments to pass to docker buildx build"
- echo " -b, --base      Docker base image"
  echo " -t, --tag       Docker image tag"
 }
 
@@ -29,6 +28,7 @@ parse_flags() {
 
       -a | --args)
         build_arguments="$2"
+        echo "[INFO] Docker build arguments: $build_arguments"
         shift
         shift
         ;;
@@ -48,13 +48,20 @@ parse_flags() {
   done
 }
 
-docker-build() {
+build() {
 
     docker_tag="--tag $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$image_tag"
 
     if [[ "$PUSH_TO_DOCKERHUB" == "true" ]]; then
         docker_tag+=" --tag docker.io/$image_tag"
     fi
+
+  echo "[INFO] docker buildx build \
+            --push \
+            --platform ${PLATFORMS} \
+            --no-cache \
+            $docker_tag \
+            $build_arguments"
 
     docker buildx build \
             --push \
@@ -74,4 +81,4 @@ parse_flags "$@"
 echo "[INFO] Docker build arguments: $build_arguments"
 echo "[INFO] Docker image tag: $image_tag"
 
-docker-build
+build
