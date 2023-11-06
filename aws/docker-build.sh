4112ab03c0eda47d6eb553c8b6f455839d42451b
Sam <109683132+kernelsam@users.noreply.github.com>
2023-11-06 09:59:10 -0800

    #57 Push Staging Images to ECR (#61)
    * #57 codebuild push staging images to ECR
    
    * #57 test
    
    * #57 test
    
    * #57 debug
    
    * #57 debug
    
    * #57 full script path
    
    * #57 update codebuild array handling
    
    * #57 debug
    
    * #57 bash default shell
    
    * #57 debug
    
    * #57 debug
    
    * #57 install buildx
    
    * #57 cleanup
    
    * #57 typo
    
    * #57 lowercase os and change arch output
    
    * #57 debugt
    
    * #57 debugt
    
    * #57 copy binary to docker plugin dir
    
    * #57 debug
    
    * #57 cleanup
    
    * #57 verify docker, add context
    
    * #57 test generic build
    
    * #57 dockerhub login
    
    * #57 switch to buildx
    
    * #57 add platforms
    
    * #57 add qemu
    
    * #57 buildx create
    
    * #57 test added params
    
    * #57 test added params
    
    * #57 test added params
    
    * #57 change order of operations
    
    * #57 debug build args
    
    * #57 debug build args
    
    * #57 debug build args
    
    * #57 revert debug
    
    * #57 switch to public ecr repo
    
    * #57 only build runtime, don't push
    
    * #57 typo
    
    * #57 pull local image
    
    * #57 debug
    
    * #57 debug
    
    * #57 debug
    
    * #57 cleanup
    
    * devops-142 skip quotes on build args
    
    * #57 test
    
    * #57 test
    
    * #57 test
    
    * #57 test
    
    * #57 test
    
    * #57 test
    
    * #57 test
    
    * #57 test
    
    * #57 test
    
    * #57 test
    
    * test
    
    * add back parse
    
    * #57 cleanup and more error handling

---
+++ aws/docker-build.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+# exit when any command fails
+set -e
+
+# Default variable values
+build_arguments=""
+image_tag=""
+
+# Function to display script usage
+usage() {
+ echo "Usage: $0 [OPTIONS]"
+ echo "PreReq: Dockerfile to build should be in the current working directory"
+ echo "Options:"
+ echo " -h, --help      Display this help message"
+ echo " -a, --args      Additional build arguments to pass to docker buildx build"
+ echo " -t, --tag       Docker image tag"
+}
+
+# Function to parse input flags
+parse_flags() {
+  while [ $# -gt 0 ]; do
+    case $1 in
+
+      -h | --help)
+        usage
+        exit 0
+        ;;
+
+      -a | --args)
+        build_arguments="$2"
+        shift
+        shift
+        ;;
+
+      -t | --tag)
+        image_tag="$2"
+        shift
+        shift
+        ;;
+
+      *)
+        echo "Invalid option: $1" >&2
+        usage
+        exit 1
+        ;;
+    esac
+  done
+}
+
+build() {
+
+    docker_tag="--tag public.ecr.aws/$image_tag"
+
+    if [[ "$PUSH_TO_DOCKERHUB" == "true" ]]; then
+        docker_tag+=" --tag docker.io/$image_tag"
+    fi
+
+    echo "[INFO] docker buildx build \
+            --push \
+            --platform ${PLATFORMS} \
+            --no-cache \
+            ${build_arguments} \
+            ${docker_tag} \
+            ."
+
+    # shellcheck disable=SC2086
+    docker buildx build \
+            --push \
+            --platform "${PLATFORMS}" \
+            --no-cache \
+            ${build_arguments} \
+            ${docker_tag} \
+            .
+
+}
+
+#########################################
+# Main
+#########################################
+
+parse_flags "$@"
+
+echo "[INFO] Docker build arguments: $build_arguments"
+echo "[INFO] Docker image tag: $image_tag"
+
+build
